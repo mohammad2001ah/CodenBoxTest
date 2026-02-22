bd5542aa0965d55f8ebf532b5975cf3af41b3113
mohammad2001ah <mah.2001.ah@gmail.com>
2026-02-22 23:29:34 +0300

    new code

--- src/MyTestCase/CodeBoxTest.java
+++ src/MyTestCase/CodeBoxTest.java
@@ -9,119 +9,124 @@ import java.util.Random;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class CodeBoxTest {
-	
-	WebDriver driver=new EdgeDriver();
-	String myWebSite="https://codenboxautomationlab.com/practice/";
-	String ParentWindow=driver.getWindowHandle();
-	
+
+	WebDriver driver = new EdgeDriver();
+	String myWebSite = "https://codenboxautomationlab.com/practice/";
+
 	Random rand = new Random();
-	
+
 	@BeforeTest
 	public void MySetUp() {
 		driver.get(myWebSite);
+		String ParentWindow = driver.getWindowHandle();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 	}
-	
-	@Test(priority = 1,enabled = false)
+
+	@Test(priority = 1, enabled = false)
 	public void RadioButton() {
-		List<WebElement> radioButton=driver.findElements(By.className("radioButton"));
-		if(radioButton.size()>0) {
-			int randomValue=rand.nextInt(radioButton.size());
-			WebElement randomeValueButton=radioButton.get(randomValue);
-			randomeValueButton.click();
-		}
+		WebElement radioButtonDiv = driver.findElement(By.id("radio-btn-example"));
+		radioButtonDiv.findElements(By.tagName("input")).get(0).click();
+		boolean ActualResult = radioButtonDiv.findElements(By.tagName("input")).get(0).isSelected();
+		boolean ExpetedResult = true;
+		Assert.assertEquals(ActualResult, ExpetedResult);
 	}
-	
-	@Test(priority = 2,enabled = false)
-	public void PrintCountries() {
-		WebElement Countries=driver.findElement(By.id("autocomplete"));
-		Countries.sendKeys("Jordan");
+
+	@Test(priority = 2, enabled = false)
+	public void PrintCountries() throws InterruptedException {
+		String[] countries = { "jo", "sy", "ir" };
+		int randomIndex = rand.nextInt(countries.length);
+		String country = countries[randomIndex];
+
+		driver.findElement(By.id("autocomplete")).sendKeys(country);
+		Thread.sleep(1000);
+		driver.findElement(By.id("autocomplete")).sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
+		Thread.sleep(3000);
 	}
-	
-	@Test(priority = 3,enabled = false)
-	public void DropDown() {
-		WebElement staticBox=driver.findElement(By.id("dropdown-class-example"));
-		Select Static =new Select(staticBox);
-		List<WebElement> Option= Static.getOptions();
-		int randOption=rand.nextInt(Option.size()-1);
-		Static.selectByIndex(randOption);
+
+	@Test(priority = 3, enabled = false)
+	public void DropDown() throws InterruptedException {
+		WebElement SelectTag = driver.findElement(By.id("dropdown-class-example"));
+		Select mySelector = new Select(SelectTag);
+		mySelector.selectByIndex(1);
+		Thread.sleep(2000);
+		mySelector.selectByVisibleText("Appium");
+		Thread.sleep(2000);
+		mySelector.selectByValue("option3");
+
 	}
-	
-	@Test(priority = 4,enabled = false)
+
+	@Test(priority = 4, enabled = false)
 	public void CheckBoxSelect() {
-		List<WebElement> checkButton=driver.findElements(By.xpath("//input[@type='checkbox']"));
-		if(checkButton.size()>0) {
-			int randomCheckOption=rand.nextInt(checkButton.size());
-			WebElement randomCheck=checkButton.get(randomCheckOption);
-			randomCheck.click();
+		WebElement CheckboxDiv = driver.findElement(By.id("checkbox-example"));
+		List<WebElement> AllCheckBoxes = CheckboxDiv.findElements(By.tagName("input"));
+		for (int i = 0; i < AllCheckBoxes.size(); i++) {
+			AllCheckBoxes.get(i).click();
+			Assert.assertEquals(AllCheckBoxes.get(i).isSelected(), true);
 		}
 	}
-	
-	@Test(priority=5)
+
+	@Test(priority = 5, enabled = false)
 	public void OpenWindow() throws InterruptedException {
-		WebElement OpenWindowButton=driver.findElement(By.id("openwindow"));
-		OpenWindowButton.click();
-		Set<String> allwindows=driver.getWindowHandles();
-		for(String window:allwindows) {
-			if (!window.equals(ParentWindow)) {
-				driver.switchTo().window(window);
-				System.out.println(driver.getTitle());
-				Thread.sleep(1000);
-				driver.close();
-			}
-		}
+		WebElement openWindowButton = driver.findElement(By.id("openwindow"));
+		openWindowButton.click();
+		Set<String> handels = driver.getWindowHandles();
+		List<String> allTabs = new ArrayList<>(handels);
+		driver.switchTo().window(allTabs.get(1));
+		System.out.println(driver.getTitle());
+		driver.switchTo().window(allTabs.get(0));
+		System.out.println(driver.getTitle());
 	}
-	
-//	@Test(priority=6,enabled = false)
-//	public void OpenWindow() throws InterruptedException {
-//		WebElement OpenWindowButton=driver.findElement(By.id("openwindow"));
-//		OpenWindowButton.click();
-//		List<String> windows=new ArrayList<>(driver.getWindowHandles());
-//		Thread.sleep(3000);
-//		String mainWindow=windows.get(0);
-//		 int index=0;
-//		 boolean flag=true;
-//		 while(flag) {
-//			 if(index >= windows.size()) {
-//				 flag=false;
-//				 break;
-//			 }
-//			 String newWindow=windows.get(index);
-//			 if(!newWindow.equals(mainWindow)) {
-//				 driver.switchTo().window(newWindow);
-//				 driver.close();
-//			 }
-//			 index++;	 
-//		 } 
-//	}
-	
-	@Test(priority = 7,enabled = false)
+
+	@Test(priority = 6, enabled = false)
 	public void NewTab() throws InterruptedException {
-		WebElement OpenButton=driver.findElement(By.id("opentab"));
-		OpenButton.click();
-		String ParentTab=driver.getWindowHandle();
-		System.out.println(ParentTab);
-		Set<String>allWindow=driver.getWindowHandles();
-		for(String window:allWindow) {
-			if (!window.equals(ParentTab)) {
-				driver.switchTo().window(window);
-				System.out.println(driver.getTitle());
-				Thread.sleep(1000);
-				driver.close();
-			}
-			
-		}
-		
+		driver.findElement(By.id("opentab")).click();
+		Set<String> handels = driver.getWindowHandles();
+		List<String> allTabs = new ArrayList<>(handels);
+		driver.switchTo().window(allTabs.get(1));
+		Thread.sleep(2000);
+		System.out.println(driver.getTitle());
+		driver.switchTo().window(allTabs.get(0));
 	}
-	
+
+	@Test(priority = 7, enabled = false)
+
+	public void AlertAndConfirm() throws InterruptedException {
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,500)");
+		String myname = "ahmad";
+		driver.findElement(By.id("name")).sendKeys(myname);
+
+		driver.findElement(By.id("confirmbtn")).click();
+		Thread.sleep(2000);
+		System.out.println(driver.switchTo().alert().getText().contains(myname));
+		boolean ActualValue = driver.switchTo().alert().getText().contains(myname);
+		Assert.assertEquals(ActualValue, true);
+		driver.switchTo().alert().dismiss();
+	}
+
+	@Test(priority = 8,enabled = false)
+	public void TheTable() {
+
+		WebElement TheTable = driver.findElement(By.id("product"));
+
+		List<WebElement> AllData = TheTable.findElements(By.tagName("td"));
+
+		System.out.println();
+		driver.findElement(By.id("name")).sendKeys(AllData.get(0).getText());
+	}
+
 }
